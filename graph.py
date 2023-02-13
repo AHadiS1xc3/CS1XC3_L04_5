86a27cde92a683f06a3dcf94609efdbdb0e5e214
Abdullah Khan <abdullahmoves@gmail.com>
2023-02-13 15:24:42 -0500

    added created rand graph function and fixed BFS3 and DFS3

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 from collections import deque
-
+import random
 #Undirected graph using an adjacency list
 class Graph:
 
@@ -64,7 +64,6 @@ def DFS(G : Graph, node1, node2):
 
 
 def BFS3 (G: Graph , node1: int  ):
-
     path_pred = {}
     Q = deque([node1])
     marked = {node1 : True}
@@ -75,35 +74,27 @@ def BFS3 (G: Graph , node1: int  ):
         current_node = Q.popleft()
         for node in G.adj[current_node]:
             if not marked[node]:
-                path_pred[node + 1] = current_node + 1
+                path_pred[node] = current_node 
                 Q.append(node)
                 marked[node] = True
     return path_pred
 
 
-
+#DFS3
 def DFS3(G : Graph, node1):
     S = [node1]
     pred = {}
     marked = {}
     for node in G.adj:
         marked[node] = False
-
     while len(S) != 0:
         current_node = S.pop()
-
         if not marked[current_node]:
-
-
             marked[current_node] = True
-            
-            print(G.adj[current_node])
             for node in G.adj[current_node]:
-
                 if not marked[node]:
-                    pred[node +1] = current_node + 1
+                    pred[node] = current_node 
                 S.append(node)
-
     return pred
 
 #has_cycle
@@ -137,53 +128,56 @@ def is_connected(G):
     dfs(list(G.adj.keys())[0], marked)
     return len(marked) == len(G.adj)
 
+# Created random graph
+def create_random_graph(i, j):
+    G = Graph(i)
+    for _ in range(j):
+        rand1 = random.randint(0,i - 1)
+        rand2 = random.randint(0,i - 1)    
+        while ( rand2 in G.adj[rand1]):
+            print (rand1,rand2)
+            rand1 = random.randint(0,i - 1)
+            rand2 = random.randint(0,i - 1)    
+        G.add_edge (rand1, rand2)
+    return G
+# Created random graph
+""" The problem with the create random
+    function above is that it can potentially
+    run into an infinite while loop. 
+    This function is more safe then that version 
+    beacuse it just crashes if too many edges 
+    are added
+"""
+def create_rand_graph_safe (num_nodes, num_edges):
+    lst_tups = []
+    G = Graph(num_nodes)
+    for x in range (num_nodes):
+        for  y in range (x , num_nodes):
+            lst_tups.append((x,y))
+    for i  in range (num_edges):
 
-def paths (G:Graph, node1 ):
-    S = [node1]
-    marked = {}
-    for node in G.adj:
-        marked[node] = False
-    while len(S) != 0:
-        current_node = S.pop()
-        if not marked[current_node]:
-            marked[current_node] = True
-            for node in G.adj[current_node]:
-                S.append(node)
-    lstnode = [k  for k in marked if marked[k] ]
-    return lstnode
+        if len(lst_tups) == 0 :
+            print ("You added to many edges.")
+        tup = lst_tups.pop(random.randint(0, len(lst_tups) - 1))
+        x = tup[0]
+        y = tup[1]
+        G.add_edge(x,y)
+    return G
 
-def isCycle (G:Graph , node1):
-    
-    p_node1 = paths(G,node1)
+if __name__ == "main":
+    test = Graph(6)
 
-    for n1 in p_node1:
-        for n2 in p_node1:
-            if G.are_connected(n1,n2):
-                return True
-    return False
+    test.add_edge(0,1)
+    test.add_edge(1,2)
+    test.add_edge(2,4)
+    test.add_edge(4,0)
 
-
-def has_cyclePrint (G: Graph):
-    for node in G.adj:
-        print(node,isCycle(G,node))
-        
-    return False
-
-
-test = Graph(6)
-
-test.add_edge(0,1)
-test.add_edge(1,2)
-test.add_edge(2,4)
-test.add_edge(4,0)
-
-#test.add_edge(1,3)
-#test.add_edge(2,4)
-#test.add_edge(2,3)
-#test.add_edge(3,4)
-#test.add_edge(3,5)
-print(test)
-print (BFS3(test,0))
-print(DFS3(test,0))
-has_cyclePrint(test)
-print((has_cycle(test)))
+    #test.add_edge(1,3)
+    #test.add_edge(2,4)
+    #test.add_edge(2,3)
+    #test.add_edge(3,4)
+    #test.add_edge(3,5)
+    print(test)
+    print (BFS3(test,0))
+    print(DFS3(test,0))
+    print((has_cycle(test)))
