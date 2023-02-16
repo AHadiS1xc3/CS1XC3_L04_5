3240bf1c87acee985fc9989056fd8fd5f832f1d8
Abdullah Khan <abdullahmoves@gmail.com>
2023-02-15 23:34:50 -0500

    finished write up for experiment 1

--- exp1.py
+++ exp1.py
@@ -3,14 +3,23 @@ import graph as g
 from tqdm import tqdm
 
 
+def gen_all_edges (num_nodes):
 
-def run_tests(epochs, num_nodes, tot_edges, label):
+    lst_tups = []
+    for x in range (num_nodes):
+        for  y in range (x , num_nodes):
+            lst_tups.append((x,y))
+    return lst_tups
+
+
+
+def run_tests(epochs, num_nodes, tot_edges, lst_tups  ,label):
     probs     = []
     # note u should delete the tqdm package
     for num_edges in  tqdm(range (tot_edges)):
         num_cycs = 0
         for _ in range(epochs):
-            graph = g.create_rand_graph_safe(num_nodes,num_edges)
+            graph = g.create_rand_graph_safe_fast(num_nodes,num_edges,lst_tups.copy())
             if g.has_cycle(graph):
                 num_cycs += 1
                 #print(graph.adj)
@@ -19,14 +28,16 @@ def run_tests(epochs, num_nodes, tot_edges, label):
     plt.plot(probs, label=label)
 
 
-epochs    = 1000
+epochs    = 10000
 num_nodes_1 = 100
 num_nodes_2 = 90
 num_nodes_3 = 80
 
-run_tests(epochs,num_nodes_1, num_nodes_1 -1 , "size: 100")
-run_tests(epochs,num_nodes_2, num_nodes_2 -1 , "size: 90")
-run_tests(epochs,num_nodes_3, num_nodes_3 -1 , "size: 80")
+
+
+run_tests(epochs,num_nodes_1, num_nodes_1 -1 , gen_all_edges(num_nodes_1) ,"size: 100")
+run_tests(epochs,num_nodes_2, num_nodes_2 -1 , gen_all_edges(num_nodes_2),"size: 90")
+run_tests(epochs,num_nodes_3, num_nodes_3 -1 , gen_all_edges(num_nodes_3),"size: 80")
 
 plt.legend(loc="upper left")
 plt.title("Edge vs Cycle Probability")
