edcfda78c6ec04e0c6da32f4b8aad804689ed099
Angel <98554114+ziyuangelli@users.noreply.github.com>
2023-02-19 13:47:11 -0500

    Update graph.py
    added professors update

--- graph.py
+++ graph.py
@@ -105,6 +105,34 @@ def DFS3(G : Graph, node1):
                 S.append(node)
     return pred
 
+def add_to_each(sets, element):
+    copy = sets.copy()
+    for set in copy:
+        set.append(element)
+    return copy
+
+def power_set(set):
+    if set == []:
+        return [[]]
+    return power_set(set[1:]) + add_to_each(power_set(set[1:]), set[0])
+
+def is_vertex_cover(G, C):
+    for start in G.adj:
+        for end in G.adj[start]:
+            if not(start in C or end in C):
+                return False
+    return True
+
+def MVC(G):
+    nodes = [i for i in range(G.get_size())]
+    subsets = power_set(nodes)
+    min_cover = nodes
+    for subset in subsets:
+        if is_vertex_cover(G, subset):
+            if len(subset) < len(min_cover):
+                min_cover = subset
+    return min_cover
+
 #has_cycle
 def has_cycle(G):
     def DFSCycleChecker(node1, parent, marked):
